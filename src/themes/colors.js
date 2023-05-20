6a8f9e79569eb6a60bd397c88fd520593630e5ae
y79tian <y79tian@uwaterloo.ca>
2023-05-19 19:25:03 -0700

    add more theme colors

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -1,13 +1,37 @@
-import { theme } from 'twin.macro';
+import { theme as twTheme } from 'twin.macro';
 
 export const LightColors = {
-	twText: theme`colors.neutral.900`,
-	twBackground: theme`colors.indigo.50`,
-	twIndigo: theme`colors.indigo.400`,
+  twText: twTheme`colors.neutral.900`,
+  twTextRgba: '23, 23, 23',
+
+  twSubText: twTheme`colors.neutral.500`,
+  twSubTextRgba: '115, 115, 115',
+
+  twBackground: twTheme`colors.neutral.100`,
+  twBackgroundRgba: '245, 245, 245',
+
+  twLink: twTheme`colors.indigo.600`,
+
+  twIndigo: twTheme`colors.indigo.400`,
+  twIndigoDark: twTheme`colors.indigo.500`,
+
+  twWave: twTheme`colors.sky.500`,
 };
 
 export const DarkColors = {
-	twText: theme`colors.indigo.50`,
-	twBackground: theme`colors.neutral.900`,
-	twIndigo: theme`colors.indigo.500`,
+  twText: twTheme`colors.slate.50`,
+  twTextRgba: '248, 250, 252',
+
+  twSubText: twTheme`colors.neutral.400`,
+  twSubTextRgba: '163, 163, 163',
+
+  twBackground: twTheme`colors.neutral.800`,
+  twBackgroundRgba: '38, 38, 38',
+
+  twLink: twTheme`colors.indigo.300`,
+
+  twIndigo: twTheme`colors.indigo.500`,
+  twIndigoDark: twTheme`colors.indigo.300`,
+
+  twWave: twTheme`colors.cyan.500`,
 };
