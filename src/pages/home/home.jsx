e283968ec0123958e897fa671fd69582470ad47b
y79tian <y79tian@uwaterloo.ca>
2023-05-19 19:25:24 -0700

    add navbar to home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Page } from './home.styles';
+import React from "react";
+import { Page } from "./home.styles";
+import { NavBar } from "../../shard";
 
 const Home = () => {
-	return <Page>Home</Page>;
+  return <Page><NavBar/></Page>;
 };
 
 export default Home;
