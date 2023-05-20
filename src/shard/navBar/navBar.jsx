a112e2b26f3686caf2001c497d64f8e70ceb11b6
y79tian <y79tian@uwaterloo.ca>
2023-05-19 19:24:00 -0700

    initialize the navbar component

---
+++ src/shard/navBar/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './navBar.styles'
+import Logo from '../logo/logo'
+
+const NavBar = () => {
+  return (
+    <Container><Logo/></Container>
+  )
+}
+
+export default NavBar
---
+++ src/shard/navBar/navBar.styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 10rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+`;
